2ef0e628e8cd2f4ef54d4fda4498831424d40959
Julia Keller <juliakeller.vk@gmail.com>
2026-09-17 23:28:01 +0200

    feat: configure admin for marketplace models

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "reviewer",
+        "business_user",
+        "rating",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("rating",)
+    search_fields = (
+        "reviewer__username",
+        "business_user__username",
+        "description",
+    )
---
+++ reviews/tests/test_admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.test import TestCase
+
+from reviews.models import Review
+
+
+class ReviewAdminTests(TestCase):
+    def test_review_is_registered_in_admin(self):
+        self.assertIn(Review, admin.site._registry)
